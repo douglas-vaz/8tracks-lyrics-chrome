4829543b65326d5e15585260bb1745345c62a3c6
douglas-vaz <douglas1992@gmail.com>
2012-12-10 10:32:40 +0530

    Fixed lyrics height, scroll on overflow

--- lyrics_jq.js
+++ lyrics_jq.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){  
   console.log("JQuery loaded");
   $("#sidebar").append('<input type="button" id="getLyrics" value="Get"/>');
-  $("#sidebar").append('<div id = "lyrics">Lyrics</div>');
+  $("#sidebar").append('<div id = "lyrics" style="overflow-y: auto; width: 301px; height: 400px;">Lyrics</div>');
   
   $("#getLyrics").click(function() {
   console.log("Handler for .click() called.");
@@ -12,7 +12,7 @@ $(document).ready(function(){
   a = a.replace(/ /gi,'_');
   
   console.log(t + " - " + a);
-  
+  //$("#lyrics").css("overflow-y","auto") 
   $("#lyrics").load("http://lyrics.wikia.com/api.php?artist=" +a+ "&song=" +t+ "&fmt=json&X-Wikia-API-Key=d728c02d80061d1dd54006a9924c1e7a5243f539",function(){
     var text = $('#lyrics').html();
     var url = text.substring(text.search('http'),text.search('}')-2);
